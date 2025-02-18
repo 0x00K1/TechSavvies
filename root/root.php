247eadc517184f4088576264127664c0b0a08e32
HameedJHD <homedjahdali@gmail.com>
2025-02-19 01:13:58 +0300

    1- Fixed the "assets\images\LogoName.webp" since since it had a big hgitbox making clicking on it hard when trying to interact with admin sidebar items
    2- Added admin sidebar in the main.css file
    
    3- created basic sidebar in root/root.php
    
    Q. how should i implement the tasks in the page?
        a. make another php files like "add.php" and "delete.php" etc.
        b. Make containers using <div> and making it appear and disappear when selecting

--- root/root.php
+++ root/root.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html5>
 <html>
     <head>
-    <link rel="stylesheet" href="..\assets\css\main.css">
+        <link rel="icon" href="..\assets\icons\Logo.ico">
+        <link rel="stylesheet" href="..\assets\css\main.css">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
         <title>Admin Page</title>
     </head>
     <body>
+        <!-- Header Section -->
         <?php include('..\assets\php\header.php'); ?>
-        
-</body>
+
+        <!-- Sidebar section -->
+        <div class="sidebar">
+            <ul>
+                <!-- STILL NOT CLEAR WHETHER I WILL MAKE ANOTHER PAGES FOR EVERY FUNCTION OR ADD FUNCTIONS IN <div></div> CONTAINERS -->
+                <li><a href="">Add Item</a></li> 
+                <li><a href="">Remove Item</a></li>
+                <li><a href="">Edit Item</a></li>
+            </ul>
+        </div>
+    </body>
 </html> 
