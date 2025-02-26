a500d7429754d6a4dd0be8564b466e721af1d2eb
HameedJHD <homedjahdali@gmail.com>
2025-02-26 17:02:13 +0300

    icons

--- assets/php/toolbar.php
+++ assets/php/toolbar.php
@@ -1,6 +1,6 @@
 <div class="toolbar">
     <nav class="nav">
-        <button id="EditPro_button"><i class="Product_icon"></i><span>Edit Products</span></button> 
+        <button id="EditPro_button"><img src="..\assets\icons\Orders.svg"><span>Edit Products</span></button> 
         <button id="EditUser_button">Edit User</button>
         <button id="Orders">Orders</button>
     </nav>
