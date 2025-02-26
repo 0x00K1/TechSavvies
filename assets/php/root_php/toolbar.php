6aa70efd8644cf2351ff9edf35cc8ac5bbe43020
HameedJHD <homedjahdali@gmail.com>
2025-02-26 17:16:42 +0300

    adsded icons

--- assets/php/root_php/toolbar.php
+++ assets/php/root_php/toolbar.php
@@ -1,7 +1,7 @@
 <div class="toolbar">
     <nav class="nav">
-        <button id="EditPro_button"><img src="..\assets\icons\Orders.svg"><span>Edit Products</span></button> 
-        <button id="EditUser_button">Edit User</button>
-        <button id="Orders">Orders</button>
+        <button id="EditPro_button"><img class="b-icon" src="..\assets\icons\Product.svg"><span>Products Management</span></button> 
+        <button id="EditUser_button"><img class="b-icon" src="..\assets\icons\User.svg"><span>Users Management</span></button>
+        <button id="Orders"><img class="b-icon" src="..\assets\icons\Orders.svg"><span>Orders</span></button>
     </nav>
 </div>
