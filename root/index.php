fe3f41aa190158d63127b835b943edc9645e58a4
Ananas0dev <ananasgamear@gmail.com>
2025-02-28 20:29:17 +0300

    first push in ramadan edit button new issue 	modified:   assets/css/root.css 	deleted:    assets/php/root_php/orders.php 	modified:   assets/php/root_php/reviews.php 	deleted:    assets/php/root_php/transactions.php 	modified:   root/index.php

--- root/index.php
+++ root/index.php
@@ -49,8 +49,8 @@
                         <button id="addProPopup_button" class="addProPopup_button_style" type="button"
                             onclick="addProPopup()">Add</button>
                         <div class="search_div">
-                            <input class="search-field_style" name="search_field" id="search_field" type="text"
-                                placeholder="Search" />
+                            <input class="search-field_style" name="product_search_field" id="search_field" type="text"
+                                placeholder="Search.. attribute:key ex(name:mustafa)" />
                             <input class="search-button_style" name="search_button" id="search_button" type="submit"
                                 value="Search" />
                             <span>Filter:</span>
@@ -105,6 +105,32 @@
                                                 </div>
                                             </td>
                                         </tr>
+                                        <tr>
+                                            <td>ID</td>
+                                            <td>Name</td>
+                                            <td>Price</td>
+                                            <td>Stock</td>
+                                            <td>Category</td>
+                                            <td>Updated Date</td>
+                                            <td>
+                                                <div id="buttons_table_display" class="buttons_table_style"
+                                                    style="display:block;">
+                                                    <button name="remove_product_button" id="remove_product_button"
+                                                        class="remove_product_button_style" type="button"
+                                                        onclick="confirmationPopup()">Remove</button>
+                                                    <button name="edit_product_button" id="edit_product_button"
+                                                        class="edit_product_button_style" type="button"
+                                                        onclick="product_edit_button()">Edit</button>
+                                                </div>
+                                                <div id="product_edit_display" class="product_edit_display_style"
+                                                    style="display:none;">
+                                                    <button id="product_cancel_edit" onclick="product_cancel_edit()"
+                                                        class="product_cancel_edit_style" type="button">Cancel</button>
+                                                    <input id="product_confirm_edit" class="product_confirm_edit_style"
+                                                        type="submit" value="Confirm" />
+                                                </div>
+                                            </td>
+                                        </tr>
                                     </tbody>
                             </table>
                         </div>
@@ -112,10 +138,194 @@
                         <!-- <- submit button-->
                     </form>
                 </div>
+                <!-- _________________________________________
+                    orders page
+                    ________________________________________-->
 
-                <?php include('..\assets\php\root_php\orders.php');?>
-                <?php include('..\assets\php\root_php\transactions.php');?>
-                <?php include('..\assets\php\root_php\reviews.php');?>
+                <div id="orders_display" class="content">
+                    <div class="search_div">
+                        <input class="search-field_style" name="product_search_field" id="search_field" type="text"
+                            placeholder="Search.. attribute:key ex(name:mustafa)" />
+                        <input class="search-button_style" name="search_button" id="search_button" type="submit"
+                            value="Search" />
+                        <span>Filter:</span>
+                        <select name="filter_value" id="filter_value" class="filter_value_style"
+                            placeholder="Chose a value">
+                            <option value="">None</option>
+                            <option name="f_category" value="category">Category</option>
+                            <option name="f_price" value="price">Price</option>
+                            <option name="f_stock" value="stock">Stock</option>
+                            <option name="f_size" value="size">Size</option>
+                            <option name="f_update_date" value="update_date">update Date</option>
+                        </select>
+                        <button name="order_toggler" id="order_toggler">^</button>
+                        <!-- some crazy js for ico change-->
+                    </div>
+                    <div class="div">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Order ID</th>
+                                    <th>Order Date</th>
+                                    <th>Product ID</th>
+                                    <th>Product Name</th>
+                                    <th>Quantity</th>
+                                    <th>Price per Unit</th>
+                                    <th>Total Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <!-- 
+                COMMENT FOR DATABSE FILL (REMOVE FROM LINE 17 AND LINE 36 WHEN READY FOR IMPLEMENTING)
+            <?php if ($orders): ?>
+                    <?php foreach ($orders as $order): ?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($order['order_id']); ?></td>
+                            <td><?php echo htmlspecialchars($order['order_date']); ?></td>
+                            <td><?php echo htmlspecialchars($order['product_id']); ?></td>
+                            <td><?php echo htmlspecialchars($order['product_name']); ?></td>
+                            <td><?php echo htmlspecialchars($order['quantity']); ?></td>
+                            <td><?php echo htmlspecialchars(number_format($order['price_per_unit'], 2)); ?></td>
+                            <td><?php echo htmlspecialchars(number_format($order['quantity'] * $order['price_per_unit'], 2)); ?>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <tr>
+                        <td colspan="7">No recent orders found.</td>
+                    </tr>
+                <?php endif; ?> -->
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!--ADD PHP CODE FOR DATABASE FETCH-->
+
+
+                <!-- _________________________________________
+                    transactions page
+                    ________________________________________-->
+                <div class="content" id="Transactions_display">
+                    <div class="search_div">
+                        <input class="search-field_style" name="product_search_field" id="search_field" type="text"
+                            placeholder="Search.. attribute:key ex(name:mustafa)" />
+                        <input class="search-button_style" name="search_button" id="search_button" type="submit"
+                            value="Search" />
+                        <span>Filter:</span>
+                        <select name="filter_value" id="filter_value" class="filter_value_style"
+                            placeholder="Chose a value">
+                            <option value="">None</option>
+                            <option name="f_category" value="category">Category</option>
+                            <option name="f_price" value="price">Price</option>
+                            <option name="f_stock" value="stock">Stock</option>
+                            <option name="f_size" value="size">Size</option>
+                            <option name="f_update_date" value="update_date">update Date</option>
+                        </select>
+                        <button name="order_toggler" id="order_toggler">^</button>
+                        <!-- some crazy js for ico change-->
+                    </div>
+                    <div class="div">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Payment ID</th>
+                                    <th>Order ID</th>
+                                    <th>User ID</th>
+                                    <th>Username</th>
+                                    <th>Payment Method</th>
+                                    <th>Payment Status</th>
+                                    <th>Transaction ID</th>
+                                    <th>Amount</th>
+                                    <th>Created At</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- COMMENT FOR DATABSE FILL (REMOVE FROM LINE 17 AND LINE 36 WHEN READY FOR IMPLEMENTING)
+            <?php if ($transactions): ?>
+                <?php foreach ($transactions as $transaction): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($transaction['payment_id']); ?></td>
+                        <td><?php echo htmlspecialchars($transaction['order_id']); ?></td>
+                        <td><?php echo htmlspecialchars($transaction['user_id']); ?></td>
+                        <td><?php echo htmlspecialchars($transaction['username']); ?></td>
+                        <td><?php echo htmlspecialchars($transaction['payment_method']); ?></td>
+                        <td><?php echo htmlspecialchars($transaction['payment_status']); ?></td>
+                        <td><?php echo htmlspecialchars($transaction['transaction_id']); ?></td>
+                        <td><?php echo htmlspecialchars(number_format($transaction['amount'], 2)); ?></td>
+                        <td><?php echo htmlspecialchars($transaction['created_at']); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <tr>
+                    <td colspan="9">No recent transactions found.</td>
+                </tr>
+            <?php endif; ?> -->
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- _________________________________________
+                    reviews page
+                    ________________________________________-->
+                <div class="div" id="Reviews_Display">
+                    <div class="content">
+                    <div class="search_div">
+                        <input class="search-field_style" name="product_search_field" id="search_field" type="text"
+                            placeholder="Search.. attribute:key ex(name:mustafa)" />
+                        <input class="search-button_style" name="search_button" id="search_button" type="submit"
+                            value="Search" />
+                        <span>Filter:</span>
+                        <select name="filter_value" id="filter_value" class="filter_value_style"
+                            placeholder="Chose a value">
+                            <option value="">None</option>
+                            <option name="f_category" value="category">Category</option>
+                            <option name="f_price" value="price">Price</option>
+                            <option name="f_stock" value="stock">Stock</option>
+                            <option name="f_size" value="size">Size</option>
+                            <option name="f_update_date" value="update_date">update Date</option>
+                        </select>
+                        <button name="order_toggler" id="order_toggler">^</button>
+                        <!-- some crazy js for ico change-->
+                    </div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Review ID</th>
+                                    <th>User ID</th>
+                                    <th>Username</th>
+                                    <th>Product ID</th>
+                                    <th>Product Name</th>
+                                    <th>Rating</th>
+                                    <th>Review Text</th>
+                                    <th>Created At</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- COMMENT FOR DATABSE FILL (REMOVE FROM LINE 17 AND LINE 36 WHEN READY FOR IMPLEMENTING)
+                <?php if ($reviews): ?>
+                    <?php foreach ($reviews as $review): ?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($review['review_id']); ?></td>
+                            <td><?php echo htmlspecialchars($review['user_id']); ?></td>
+                            <td><?php echo htmlspecialchars($review['username']); ?></td>
+                            <td><?php echo htmlspecialchars($review['product_id']); ?></td>
+                            <td><?php echo htmlspecialchars($review['product_name']); ?></td>
+                            <td><?php echo htmlspecialchars($review['rating']); ?></td>
+                            <td><?php echo htmlspecialchars($review['review_text']); ?></td>
+                            <td><?php echo htmlspecialchars($review['created_at']); ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <tr>
+                        <td colspan="8">No recent reviews found.</td>
+                    </tr>
+                <?php endif; ?>
+                -->
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
         <!-- ##################################
