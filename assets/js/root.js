4cc6407bc994909b2d0cde40d1c3c9eb00a3a1d1
Ananas0dev <ananasgamear@gmail.com>
2025-02-28 17:31:11 +0300

    completely deleted user 	modified:   assets/js/root.js 	deleted:    assets/php/root_php/users.php

--- assets/js/root.js
+++ assets/js/root.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     /*buttons*/
     const managePro_button = document.getElementById('managePro_button');
-    /*const manageUser_button = document.getElementById('manageUser_button');*/
     const Orders_button= document.getElementById('Orders_button');
     const transaction_button= document.getElementById('transactions_button');
     const review_button= document.getElementById('Reviews_button');
@@ -10,63 +9,38 @@ document.addEventListener('DOMContentLoaded', function() {
 
     /* div or displays*/
     const EditProduct = document.getElementById('EditProduct');
-    /*const EditUser = document.getElementById('EditUser');*/
     const orders_display=document.getElementById('orders_display');
     const Transaction_display = document.getElementById('Transactions_display'); // New div for transactions
     const Reviews_display = document.getElementById('Reviews_Display'); // New div for reviews
-    /* forms*//*
-    const addProduct_form =document.getElementById('addProduct_form');
-    const editUser_form =document.getElementById('editUser_form');
-    const viewUser_form= document.getElementById('viewUser_form');*/
 
     /*first time loading page */
     EditProduct.style.display = 'block';
-    /*EditUser.style.display = 'none';*/
     orders_display.style.display= 'none';
     Transaction_display.style.display = 'none'; // Initially hide
     Reviews_display.style.display = 'none'; // Initially hide
     managePro_button.classList.add('active');/*for button styles*/
     
     
-   
-    /*diale the other form*/ /*
-    disableForm(editUser_form);
-    disableForm(viewUser_form);
-    disableForm(addProduct_form);
-    */
+
     /* second on click some styling*/
     managePro_button.addEventListener('click', function() {
         EditProduct.style.display = 'block';
-        /*EditUser.style.display = 'none';*/
         orders_display.style.display= 'none';
         Transaction_display.style.display = 'none';
         Reviews_display.style.display = 'none';
         managePro_button.classList.add('active');
-        /*manageUser_button.classList.remove('active');*/
         Orders_button.classList.remove('active');
         transaction_button.classList.remove('active');
         review_button.classList.remove('active');
     });
 
-    /*manageUser_button.addEventListener('click', function() {
-        EditProduct.style.display = 'none';
-        EditUser.style.display = 'block';
-        orders_display.style.display= 'none';
-        Transaction_display.style.display = 'none';
-        Reviews_display.style.display = 'none';
-        managePro_button.classList.remove('active');
-        manageUser_button.classList.add('active');
-        Orders_button.classList.remove('active'); 
-    });*/
 
     Orders_button.addEventListener('click',function(){
         EditProduct.style.display = 'none';
-        /*EditUser.style.display = 'none';*/
         orders_display.style.display= 'block';
         Transaction_display.style.display = 'none';
         Reviews_display.style.display = 'none';
         managePro_button.classList.remove('active');
-        /*manageUser_button.classList.remove('active');*/
         Orders_button.classList.add('active');
         transaction_button.classList.remove('active');
         review_button.classList.remove('active');
@@ -74,12 +48,10 @@ document.addEventListener('DOMContentLoaded', function() {
    
     transaction_button.addEventListener('click', function() {
         EditProduct.style.display = 'none';
-        /*EditUser.style.display = 'none';*/
         orders_display.style.display = 'none';
         Transaction_display.style.display = 'block'; // Show reviews div
         Reviews_display.style.display = 'none'; // Hide transactions div
         managePro_button.classList.remove('active');
-        /*manageUser_button.classList.remove('active');*/
         Orders_button.classList.remove('active');
         transaction_button.classList.add('active');
         review_button.classList.remove('active');
@@ -87,12 +59,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     review_button.addEventListener('click', function() {
         EditProduct.style.display = 'none';
-        /*EditUser.style.display = 'none';*/
         orders_display.style.display = 'none';
         Transaction_display.style.display = 'none'; // Hide reviews div
         Reviews_display.style.display = 'block'; // Show transactions div
         managePro_button.classList.remove('active');
-        /*manageUser_button.classList.remove('active');*/
         Orders_button.classList.remove('active');
         transaction_button.classList.remove('active');
         review_button.classList.add('active');
@@ -105,25 +75,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 });
-/*
-function disableForm(formToDiable) {
-    const form = document.getElementById(formToDiable); // Replace with your form's ID
-
-    if (form) {
-        for (let i = 0; i < form.elements.length; i++) {
-            form.elements[i].disabled = true;
-        }
-    }
-}
-function enableForm(formToEnable) {
-    const form = document.getElementById(formToEnable); // Replace with your form's ID
-
-    if (form) {
-        for (let i = 0; i < form.elements.length; i++) {
-            form.elements[i].disabled = false;
-        }
-    }
-}*/
 
 
 function addProPopup(){
@@ -140,13 +91,13 @@ function confirmationPopup(){
 function closeconfirmationPopup(){
     document.getElementById("confirmationPopup_display").style.display= "none";
 }
+function product_cancel_edit(){
+    document.getElementById('product_edit_display').style.display= "block";
+    document.getElementById('buttons_table_display').style.display= "none";
+}
 
 function product_edit_button(){
  document.getElementById('product_edit_display').style.display= "block";
  document.getElementById('buttons_table_display').style.display= "none";
 }
 
-function product_cancel_edit(){
-    document.getElementById('product_edit_display').style.display= "none";
-    document.getElementById('buttons_table_display').style.display= "block";
-}
