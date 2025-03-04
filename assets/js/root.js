14ab047df2696c73da4c02f7214792c63bf9a5e4
0x00K1 <1234mohanad5999@gmail.com>
2025-03-05 02:13:18 +0300

    v0.9

--- assets/js/root.js
+++ assets/js/root.js
@@ -1,31 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     /*buttons*/
     const managePro_button = document.getElementById('managePro_button');
-    const Orders_button= document.getElementById('Orders_button');
-    const transaction_button= document.getElementById('transactions_button');
-    const review_button= document.getElementById('Reviews_button');
-
-    const search_button= document.getElementById('search_button');
+    const Orders_button = document.getElementById('Orders_button');
+    const transaction_button = document.getElementById('transactions_button');
+    const review_button = document.getElementById('Reviews_button');
+    const logout_button = document.getElementById('Logout_button');
+    const search_button = document.getElementById('search_button');
 
     /* div or displays*/
     const EditProduct = document.getElementById('EditProduct');
-    const orders_display=document.getElementById('orders_display');
+    const orders_display = document.getElementById('orders_display');
     const Transaction_display = document.getElementById('Transactions_display'); // New div for transactions
     const Reviews_display = document.getElementById('Reviews_Display'); // New div for reviews
 
-    /*first time loading page */
+    /* first time loading page */
     EditProduct.style.display = 'block';
-    orders_display.style.display= 'none';
+    orders_display.style.display = 'none';
     Transaction_display.style.display = 'none'; // Initially hide
     Reviews_display.style.display = 'none'; // Initially hide
-    managePro_button.classList.add('active');/*for button styles*/
-    
-    
+    managePro_button.classList.add('active'); /*for button styles*/
 
     /* second on click some styling*/
     managePro_button.addEventListener('click', function() {
         EditProduct.style.display = 'block';
-        orders_display.style.display= 'none';
+        orders_display.style.display = 'none';
         Transaction_display.style.display = 'none';
         Reviews_display.style.display = 'none';
         managePro_button.classList.add('active');
@@ -34,10 +32,9 @@ document.addEventListener('DOMContentLoaded', function() {
         review_button.classList.remove('active');
     });
 
-
-    Orders_button.addEventListener('click',function(){
+    Orders_button.addEventListener('click', function() {
         EditProduct.style.display = 'none';
-        orders_display.style.display= 'block';
+        orders_display.style.display = 'block';
         Transaction_display.style.display = 'none';
         Reviews_display.style.display = 'none';
         managePro_button.classList.remove('active');
@@ -45,12 +42,12 @@ document.addEventListener('DOMContentLoaded', function() {
         transaction_button.classList.remove('active');
         review_button.classList.remove('active');
     });
-   
+
     transaction_button.addEventListener('click', function() {
         EditProduct.style.display = 'none';
         orders_display.style.display = 'none';
-        Transaction_display.style.display = 'block'; // Show reviews div
-        Reviews_display.style.display = 'none'; // Hide transactions div
+        Transaction_display.style.display = 'block';
+        Reviews_display.style.display = 'none';
         managePro_button.classList.remove('active');
         Orders_button.classList.remove('active');
         transaction_button.classList.add('active');
@@ -60,34 +57,35 @@ document.addEventListener('DOMContentLoaded', function() {
     review_button.addEventListener('click', function() {
         EditProduct.style.display = 'none';
         orders_display.style.display = 'none';
-        Transaction_display.style.display = 'none'; // Hide reviews div
-        Reviews_display.style.display = 'block'; // Show transactions div
+        Transaction_display.style.display = 'none';
+        Reviews_display.style.display = 'block';
         managePro_button.classList.remove('active');
         Orders_button.classList.remove('active');
         transaction_button.classList.remove('active');
         review_button.classList.add('active');
     });
 
-    search_button.addEventListener('click', function(){
-        /*Retreives user data from DATABASe and fills it into the tabel*/
+    search_button.addEventListener('click', function() {
     });
 
-
-
+    logout_button.addEventListener('click', function() {
+        const confirmLogout = confirm("Are you sure you want to log out?");
+        if (confirmLogout) {
+            window.location.href = "/includes/cls.php";
+        }
+    });
 });
 
-
-
-
 function closeaddProPopup() {
-        document.getElementById("addProPopup_display").style.display = "none";
+    document.getElementById("addProPopup_display").style.display = "none";
 }
 
-function confirmationPopup(){
-    document.getElementById("confirmationPopup_display").style.display= "block";
+function confirmationPopup() {
+    document.getElementById("confirmationPopup_display").style.display = "block";
 }
-function closeconfirmationPopup(){
-    document.getElementById("confirmationPopup_display").style.display= "none";
+
+function closeconfirmationPopup() {
+    document.getElementById("confirmationPopup_display").style.display = "none";
 }
 
 // Define these functions in the global scope
--- root/index.php
+++ root/index.php
@@ -36,6 +36,8 @@
                         src="..\assets\icons\transaction.svg"><span>Transactions</span></button>
                 <button id="Reviews_button"><img class="b-icon"
                         src="..\assets\icons\review.svg"><span>Reviews</span></button>
+                <button id="Logout_button"><svg class="b-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M14 4L17.5 4C20.5577 4 20.5 8 20.5 12C20.5 16 20.5577 20 17.5 20H14M3 12L15 12M3 12L7 8M3 12L7 16" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                <span>Logout</span></button>
             </nav>
         </div>
         <!-- ##################################
